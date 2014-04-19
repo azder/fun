ae60e7f69091bc615ccf9dd1ea7f35d5d35c9cfe
azder <azhder@gmail.com>
2014-04-19 19:00:53 +0200

    added requirejs for the new examples-04.html and changed examples-02.js a bit

--- examples/example-02.js
+++ examples/example-02.js
@@ -1,15 +1,19 @@
 /*jshint node:true, globalstrict:true*/
 
-'use strict';
-
 /**
  * Created by azder on 2014-03-23.
  */
 
-var fun = require('../source/fun.js');
+'use strict';
+
+var fun = require('../fun.js');
+
+//noinspection JSHint
+//fun({ mixin: this });
 
 var add3 = fun.acurry(function (a, b, c) {
     return a + b + c;
 });
 
 console.log(add3, add3(2), add3(2, 5));
+
